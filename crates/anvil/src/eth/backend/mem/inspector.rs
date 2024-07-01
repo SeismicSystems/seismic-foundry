5a3458a45d3d98ef2ea730aff3d575636e06d25b
Christian Drappi <c@seismic.systems>
2024-07-01 15:22:02 -0400

    Anvil's seismic inspector should use `SyncInMemoryDb` (#14)

--- crates/anvil/src/eth/backend/mem/inspector.rs
+++ crates/anvil/src/eth/backend/mem/inspector.rs
@@ -18,7 +18,7 @@ use foundry_evm::{
 /// The [`revm::Inspector`] used when transacting in the evm
 #[derive(Clone, Debug, Default)]
 pub struct Inspector {
-    pub seismic: Option<seismic_inspector::SeismicInspector<seismic_db::CommitmentInMemoryDb>>,
+    pub seismic: Option<seismic_inspector::SeismicInspector<seismic_db::SyncInMemoryDB>>,
     pub tracer: Option<TracingInspector>,
     /// collects all `console.sol` logs
     pub log_collector: LogCollector,
@@ -46,7 +46,8 @@ impl Inspector {
     }
 
     pub fn with_seismic(mut self) -> Self {
-        self.seismic = Some(seismic_inspector::new_in_memory_seismic_inspector());
+        let db = crate::eth::SEISMIC_DB.clone();
+        self.seismic = Some(seismic_inspector::get_new_seismic_inspector(db));
         self
     }
 }
