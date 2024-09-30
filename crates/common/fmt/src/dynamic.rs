7f71b10280c50dee8252a401eb3cf24d57db856b
Christian Drappi <christiandrappi@gmail.com>
2024-09-30 16:17:43 -0400

    run cargofmt over repo

--- crates/common/fmt/src/dynamic.rs
+++ crates/common/fmt/src/dynamic.rs
@@ -1,6 +1,9 @@
 use super::{format_int_exp, format_uint_exp};
 use alloy_dyn_abi::{DynSolType, DynSolValue};
-use alloy_primitives::{aliases::{SInt, SUInt}, hex, SAddress};
+use alloy_primitives::{
+    aliases::{SInt, SUInt},
+    hex, SAddress,
+};
 use std::fmt;
 
 /// [`DynSolValue`] formatter.
@@ -70,7 +73,9 @@ impl DynValueFormatter {
                     self.tuple(tuple, f)
                 }
             }
-            &DynSolValue::Saddress(SAddress(inner)) | &DynSolValue::Sint(SInt(inner), _) | &DynSolValue::Suint(SUInt(inner), _) => write!(f, "{inner}")
+            &DynSolValue::Saddress(SAddress(inner)) |
+            &DynSolValue::Sint(SInt(inner), _) |
+            &DynSolValue::Suint(SUInt(inner), _) => write!(f, "{inner}"),
         }
     }
 
