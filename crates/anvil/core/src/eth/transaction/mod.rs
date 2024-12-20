29d36bbebca8eb68985379f7f0a4d77eee7fb851
cdrappi <c@seismic.systems>
2024-12-20 15:02:50 -0500

    rustfmt

--- crates/anvil/core/src/eth/transaction/mod.rs
+++ crates/anvil/core/src/eth/transaction/mod.rs
@@ -96,8 +96,8 @@ pub fn transaction_request_to_typed(
             is_system_transaction: other.get_deserialized::<bool>("isSystemTx")?.ok()?,
             input: input.into_input().unwrap_or_default(),
         }));
-    } else if transaction_type == Some(SeismicTransaction::TRANSACTION_TYPE)
-        || has_seismic_fields(&other)
+    } else if transaction_type == Some(SeismicTransaction::TRANSACTION_TYPE) ||
+        has_seismic_fields(&other)
     {
         return Some(TypedTransactionRequest::Seismic(TxSeismic {
             nonce: nonce.unwrap_or_default(),
@@ -121,8 +121,8 @@ pub fn transaction_request_to_typed(
         to,
     ) {
         // legacy transaction
-        (Some(0), _, None, None, None, None, None, None, _)
-        | (None, Some(_), None, None, None, None, None, None, _) => {
+        (Some(0), _, None, None, None, None, None, None, _) |
+        (None, Some(_), None, None, None, None, None, None, _) => {
             Some(TypedTransactionRequest::Legacy(TxLegacy {
                 nonce: nonce.unwrap_or_default(),
                 gas_price: gas_price.unwrap_or_default(),
@@ -134,8 +134,8 @@ pub fn transaction_request_to_typed(
             }))
         }
         // EIP2930
-        (Some(1), _, None, None, _, None, None, None, _)
-        | (None, _, None, None, Some(_), None, None, None, _) => {
+        (Some(1), _, None, None, _, None, None, None, _) |
+        (None, _, None, None, Some(_), None, None, None, _) => {
             Some(TypedTransactionRequest::EIP2930(TxEip2930 {
                 nonce: nonce.unwrap_or_default(),
                 gas_price: gas_price.unwrap_or_default(),
@@ -148,10 +148,10 @@ pub fn transaction_request_to_typed(
             }))
         }
         // EIP1559
-        (Some(2), None, _, _, _, _, None, None, _)
-        | (None, None, Some(_), _, _, _, None, None, _)
-        | (None, None, _, Some(_), _, _, None, None, _)
-        | (None, None, None, None, None, _, None, None, _) => {
+        (Some(2), None, _, _, _, _, None, None, _) |
+        (None, None, Some(_), _, _, _, None, None, _) |
+        (None, None, _, Some(_), _, _, None, None, _) |
+        (None, None, None, None, None, _, None, None, _) => {
             // Empty fields fall back to the canonical transaction schema.
             Some(TypedTransactionRequest::EIP1559(TxEip1559 {
                 nonce: nonce.unwrap_or_default(),
@@ -192,9 +192,9 @@ pub fn transaction_request_to_typed(
 }
 
 fn has_optimism_fields(other: &OtherFields) -> bool {
-    other.contains_key("sourceHash")
-        && other.contains_key("mint")
-        && other.contains_key("isSystemTx")
+    other.contains_key("sourceHash") &&
+        other.contains_key("mint") &&
+        other.contains_key("isSystemTx")
 }
 
 fn has_seismic_fields(other: &OtherFields) -> bool {
@@ -1224,12 +1224,12 @@ pub struct DepositReceipt<T = Receipt<alloy_primitives::Log>> {
 
 impl DepositReceipt {
     fn payload_len(&self) -> usize {
-        self.inner.receipt.status.length()
-            + self.inner.receipt.cumulative_gas_used.length()
-            + self.inner.logs_bloom.length()
-            + self.inner.receipt.logs.length()
-            + self.deposit_nonce.map_or(0, |n| n.length())
-            + self.deposit_receipt_version.map_or(0, |n| n.length())
+        self.inner.receipt.status.length() +
+            self.inner.receipt.cumulative_gas_used.length() +
+            self.inner.logs_bloom.length() +
+            self.inner.receipt.logs.length() +
+            self.deposit_nonce.map_or(0, |n| n.length()) +
+            self.deposit_receipt_version.map_or(0, |n| n.length())
     }
 
     /// Returns the rlp header for the receipt payload.
@@ -1331,12 +1331,12 @@ pub enum TypedReceipt<T = Receipt<alloy_primitives::Log>> {
 impl<T> TypedReceipt<T> {
     pub fn as_receipt_with_bloom(&self) -> &ReceiptWithBloom<T> {
         match self {
-            Self::Legacy(r)
-            | Self::EIP1559(r)
-            | Self::EIP2930(r)
-            | Self::EIP4844(r)
-            | Self::EIP7702(r)
-            | Self::Seismic(r) => r,
+            Self::Legacy(r) |
+            Self::EIP1559(r) |
+            Self::EIP2930(r) |
+            Self::EIP4844(r) |
+            Self::EIP7702(r) |
+            Self::Seismic(r) => r,
             Self::Deposit(r) => &r.inner,
         }
     }
@@ -1345,12 +1345,12 @@ impl<T> TypedReceipt<T> {
 impl<T> From<TypedReceipt<T>> for ReceiptWithBloom<T> {
     fn from(value: TypedReceipt<T>) -> Self {
         match value {
-            TypedReceipt::Legacy(r)
-            | TypedReceipt::EIP1559(r)
-            | TypedReceipt::EIP2930(r)
-            | TypedReceipt::EIP4844(r)
-            | TypedReceipt::EIP7702(r)
-            | TypedReceipt::Seismic(r) => r,
+            TypedReceipt::Legacy(r) |
+            TypedReceipt::EIP1559(r) |
+            TypedReceipt::EIP2930(r) |
+            TypedReceipt::EIP4844(r) |
+            TypedReceipt::EIP7702(r) |
+            TypedReceipt::Seismic(r) => r,
             TypedReceipt::Deposit(r) => r.inner,
         }
     }
@@ -1519,11 +1519,11 @@ impl Encodable2718 for TypedReceipt {
             out.put_u8(ty);
         }
         match self {
-            Self::Legacy(r)
-            | Self::EIP2930(r)
-            | Self::EIP1559(r)
-            | Self::EIP4844(r)
-            | Self::EIP7702(r) => r.encode(out),
+            Self::Legacy(r) |
+            Self::EIP2930(r) |
+            Self::EIP1559(r) |
+            Self::EIP4844(r) |
+            Self::EIP7702(r) => r.encode(out),
             Self::Deposit(r) => r.encode(out),
             Self::Seismic(r) => r.encode(out),
         }
