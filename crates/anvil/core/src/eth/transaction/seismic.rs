4405cd710470085ce0b61d8882f20dc9b169bbe1
cdrappi <c@seismic.systems>
2025-01-25 13:02:09 -0500

    better

--- crates/anvil/core/src/eth/transaction/seismic.rs
+++ crates/anvil/core/src/eth/transaction/seismic.rs
@@ -30,51 +30,20 @@ pub enum SeismicCallRequest {
     Bytes(Bytes),
 }
 
-impl Into<SeismicCallRequest> for Bytes {
-    fn into(self) -> SeismicCallRequest {
-        SeismicCallRequest::Bytes(self)
-    }
-}
-
-impl Into<SeismicCallRequest> for TransactionRequest {
-    fn into(self) -> SeismicCallRequest {
-        SeismicCallRequest::TransactionRequest(WithOtherFields::new(self))
-    }
-}
-
 impl Into<SeismicCallRequest> for TypedDataRequest {
     fn into(self) -> SeismicCallRequest {
         SeismicCallRequest::TypedData(self)
     }
 }
 
-/*
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct SeismicTxTypedData {
-    domain: Eip712Domain,
-    resolver: Resolver,
-    primary_type: String,
-    pub tx: TxSeismic,
-    signing_hash: FixedBytes<32>,
-}
-
-impl SeismicTxTypedData {
-    pub fn verify(&self, sig: &PrimitiveSignature) -> anyhow::Result<Address> {
-        let vk = sig.recover_from_prehash(&self.signing_hash)?;
-        let address = alloy_signer::utils::public_key_to_address(&vk);
-        Ok(address)
-    }
-
-    pub fn from_typed_data(data: TypedData) -> anyhow::Result<Self> {
-        let signing_hash = data.eip712_signing_hash()?;
-        let tx = serde_json::from_value(data.message)?;
-        Ok(Self {
-            domain: data.domain,
-            resolver: data.resolver,
-            primary_type: data.primary_type,
-            tx,
-            signing_hash,
-        })
+impl Into<SeismicCallRequest> for WithOtherFields<TransactionRequest> {
+    fn into(self) -> SeismicCallRequest {
+        SeismicCallRequest::TransactionRequest(self)
     }
 }
-*/
+
+impl Into<SeismicCallRequest> for Bytes {
+    fn into(self) -> SeismicCallRequest {
+        SeismicCallRequest::Bytes(self)
+    }
+}
