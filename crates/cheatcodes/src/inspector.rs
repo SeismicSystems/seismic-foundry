ee7f9027501c247c1a4ddc4b30c67e9fc6c453a0
SFYLL <sd@seismic.systems>
2025-01-27 11:59:04 +0100

    update init

--- crates/cheatcodes/src/inspector.rs
+++ crates/cheatcodes/src/inspector.rs
@@ -157,7 +157,7 @@ where
         db: &mut ccx.ecx.db as &mut dyn DatabaseExt,
         error,
         l1_block_info,
-        kernel: revm::seismic::new_test_kernel_box(&ccx.ecx.env)
+        kernel: revm::seismic::Kernel::test_default()
     };
 
     let mut evm = new_evm_with_existing_context(inner, &mut *inspector);
