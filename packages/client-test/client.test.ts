203042aa28cfb8efe7516386881d1e1549b9518b
cdrappi <c@seismic.systems>
2025-03-17 16:29:11 -0400

    will this do it

---
+++ packages/client-test/client.test.ts
@@ -0,0 +1,79 @@
+import { createPublicClient, http, type Hex } from "viem";
+import {
+  createShieldedPublicClient,
+  createShieldedWalletClient,
+  sanvil,
+} from "seismic-viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { beforeAll, afterAll, describe, test } from "bun:test";
+import {
+  setupNode,
+  testAesKeygen,
+  testAesGcm,
+  testEcdh,
+  testHkdfHex,
+  testHkdfString,
+  testRng,
+  testRngWithPers,
+  testSecp256k1,
+  testSeismicCallTypedData,
+  testSeismicTx,
+  testSeismicTxEncoding,
+  testSeismicTxTypedData,
+  testWsConnection,
+} from "seismic-viem-tests";
+
+const privateKey = process.env.PRIVATE_KEY as Hex;
+
+if (!privateKey) {
+  throw new Error("PRIVATE_KEY is not set");
+}
+
+const chain = sanvil;
+const { url, exitProcess } = await setupNode(chain);
+const account = privateKeyToAccount(privateKey);
+
+const pcParams = { chain, url };
+
+const encryptionSk = generatePrivateKey();
+const encryptionPubkey = privateKeyToAccount(encryptionSk).publicKey;
+
+describe("seismic-viem-tests", () => {
+  test("testAes", testAesKeygen);
+  test("testAesGcm", () => testAesGcm(pcParams));
+  test("testEcdh", () => testEcdh(pcParams));
+  test("testHkdfHex", () => testHkdfHex(pcParams));
+  test("testHkdfString", () => testHkdfString(pcParams));
+  test("testRng", () => testRng(pcParams, 32));
+  test("testRngWithPers", () => testRngWithPers(pcParams, 32));
+  test("testSecp256k1", () => testSecp256k1(pcParams));
+  test("testSeismicCallTypedData", () =>
+    testSeismicCallTypedData({
+      ...pcParams,
+      account,
+      encryptionPubkey,
+      encryptionSk,
+    }));
+  test("ws connection", () =>
+    testWsConnection({ chain, wsUrl: "ws://localhost:8545" })
+  );
+  test("testSeismicTx", () => testSeismicTx({ ...pcParams, account }));
+  test("testSeismicTxEncoding", () =>
+    testSeismicTxEncoding({
+      ...pcParams,
+      account,
+      encryptionPubkey,
+      encryptionSk,
+    }));
+  test("testSeismicTxTypedData", () =>
+    testSeismicTxTypedData({
+      ...pcParams,
+      account,
+      encryptionPubkey,
+      encryptionSk,
+    }));
+});
+
+afterAll(() => {
+  exitProcess();
+});
