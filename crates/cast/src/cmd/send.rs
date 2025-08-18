8b6e8734bc7c57d1ac023616942331d969323ece
Christian Drappi <c@seismic.systems>
2025-08-18 14:42:40 -0400

    Update send.rs

--- crates/cast/src/cmd/send.rs
+++ crates/cast/src/cmd/send.rs
@@ -259,8 +259,8 @@ impl SendTxArgs {
             cast_send(provider, tx, cast_async, confirmations, timeout).await
         // Case 2:
         // An option to use a local signer was provided.
-        // If we cannot successfully instantiate a local signer, then we will assume we don't
-        // have enough information to sign and we must bail.
+        // If we cannot successfully instantiate a local signer, then we will assume we don't have
+        // enough information to sign and we must bail.
         } else {
             // Retrieve the signer, and bail if it can't be constructed.
             let signer = eth.wallet.signer().await?;
