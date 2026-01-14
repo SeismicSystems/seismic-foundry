79abbf90aa493dcf438bd1c79a2ed24874d2f43e
cdrappi <c@seismic.systems>
2026-01-14 12:04:44 -0500

    dont change test utils

--- crates/test-utils/src/util.rs
+++ crates/test-utils/src/util.rs
@@ -47,7 +47,7 @@ static TEMPLATE_LOCK: LazyLock<PathBuf> =
 static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
 
 /// The default Solc version used when compiling tests.
-pub const SOLC_VERSION: &str = "0.8.30";
+pub const SOLC_VERSION: &str = "0.8.31";
 
 /// Another Solc version used when compiling tests.
 ///
