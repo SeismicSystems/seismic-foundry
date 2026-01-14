43c3d24c51bb77e830659457bd579394e5b5381f
cdrappi <c@seismic.systems>
2026-01-14 17:37:14 -0500

    sforge test: force unsafe private storage access

--- crates/forge/src/multi_runner.rs
+++ crates/forge/src/multi_runner.rs
@@ -178,7 +178,9 @@ impl MultiContractRunner {
         trace!("running all tests");
 
         // The DB backend that serves all the data.
-        let db = Backend::spawn(self.fork.take())?;
+        let mut db = Backend::spawn(self.fork.take())?;
+        // For tests, always allow private storage access
+        db.set_unsafe_private_storage(true);
 
         let find_timer = Instant::now();
         let contracts = self.matching_contracts(filter).collect::<Vec<_>>();
